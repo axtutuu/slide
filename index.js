93c97804cc9b6fc1f08f5323c65aa3768202a8a6
kawasaki-atsushi <kawasaki8910@gmail.com>
2017-01-27 12:27:33 +0900

    edit path

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var PDFController = require("pdf.js-controller");
 var container = document.getElementById("pdf-container");
 var controller = new PDFController({
     container: container,
-    pdfjsDistDir: "./node_modules/pdfjs-dist/"
+    pdfjsDistDir: "./production/pdfjs-dist/"
 });
 
 controller.loadDocument(pdfURL).then(initializedEvent).catch(function (error) {
